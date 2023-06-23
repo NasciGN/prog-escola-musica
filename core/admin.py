c78bde10712fb4062964fc1f372990b3718e7f71
NasciGN <gnascimento.job@gmail.com>
2023-06-23 13:29:40 -0400

    1.1 - Models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from core.models import Muscico, Instrumento
 # Register your models here.
+
+
+@admin.register(Muscico)
+class MusicoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'nacionalidade', 'dt_nascimento')
+    list_filter = ('nome', 'nacionalidade')
+    search_fields = ('nome', 'nacionalidade')
+
+
+@admin.register(Instrumento)
+class InstrumentoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'marca', 'modelo', 'n_serie', 'musico')
+    list_filter = ('nome', 'marca')
+    search_fields = ('nome', 'marca', 'modelo', 'n_serie', 'musico')
