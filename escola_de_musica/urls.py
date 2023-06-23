1b19ef07fac34982c2da26ca22d59432e84363e8
NasciGN <gnascimento.job@gmail.com>
2023-06-23 14:23:28 -0400

    1.2 - Template

--- escola_de_musica/urls.py
+++ escola_de_musica/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 from django.conf.urls.static import static
+from core import urls
 from escola_de_musica import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(admin.site.urls)),
+    path('', include(urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
