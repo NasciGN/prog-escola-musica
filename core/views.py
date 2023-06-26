1ae9509ac24c4dfb405eee093fb67618747ddea1
NasciGN <gnascimento.job@gmail.com>
2023-06-25 20:33:29 -0400

    1.8 - User Manager

--- core/views.py
+++ core/views.py
@@ -1,14 +1,48 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
-# Create your views here.
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 class IndexView(TemplateView):
     template_name = "index.html"
 
-class EscolaView(TemplateView):
-    template_name = "table.html"
+def registrar(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.instance.is_staff = False
+            form.instance.is_superuser = False
+            form.instance.is_active = True
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'user/registrar.html', {
+        'form': form
+    })
+
+def logar(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Bem-vindo {username}!")
+                return redirect('sinfonia:read')
+            else:
+                messages.error(request, "Nome de usuário ou senha inválidos.")
+        else:
+            messages.error(request, "Nome de usuário ou senha inválidos.")
+    else:
+        form = AuthenticationForm()
+    return render(request, template_name="user/login.html", context={"form": form})
+
 
 
 
--- escola_de_musica/urls.py
+++ escola_de_musica/urls.py
@@ -1,34 +1,21 @@
-"""
-URL configuration for escola_de_musica project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
-
+from django.contrib.auth import views as view
 from django.conf.urls.static import static
 from core import views
 from escola_de_musica import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin:index'),
     path('', views.IndexView.as_view(), name='home'),
     path('sinfonia/', include('sinfonia.urls')),
     path('instrumentos/', include('instrumento.urls')),
     path('orquestras/', include('orquestra.urls')),
     path('apresentacoes/', include('apresentacao.urls')),
     path('musico/', include('musico.urls')),
+    path('registrar/', views.registrar, name='registrar'),
+    path('login/', views.logar, name='login'),
+    path('logout/', view.LogoutView.as_view(next_page='home'), name='logout'),
 ] 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
